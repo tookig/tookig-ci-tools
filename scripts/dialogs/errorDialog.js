95ffad0cf7d44f9909608839d56b1f44ac934e6a
Christian Olsson <christian.olsson@tokig.net>
2021-07-22 13:41:55 +0200

    Fix args

--- scripts/dialogs/errorDialog.js
+++ scripts/dialogs/errorDialog.js
@@ -8,20 +8,22 @@ var dialogs = dialogs || {}; // eslint-disable-line
    * Opens up a error list dialog box
    *
    * @param  {object} args                  Error dialog parameters
-   * @param  {string} args.error            Error to show. Can be a string or an Error-like object
+   * @param  {string} args.error            Error to show. Can be a string, array with strings, or an Error-like object
    * @param  {string} [args.title='Error']  Dialog box title
    * @param  {object} [args.ok_text=OK]     Text for the OK-button
    * @param  {function} [args.okCallback]   Callback for the OK button. Optional, can also use the 'ok' function returned by textDialog
    * @return {object}
    */
   function errorDialog (args) {
-    let errors
+    let errors = []
     if (typeof args.error === 'string') {
       errors = [args.error]
     } else if (args.error instanceof AggregateError) {
       errors = args.error.errors.map(e => e.message)
     } else if (args.error instanceof Error) {
       errors = [args.error.message]
+    } else if (Array.isArray(args.error)) {
+      errors = args.error
     }
 
     const iargs = Object.assign({
